8f498480b6a0b3753926f279f506679e56456e1b
Savu Victor <victor@xprover.com>
2023-03-03 14:25:11 +0200

    Initial-cleanup fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,56 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'xProver',
-  tagline: '',
-  favicon: 'img/favicon.ico',
+  title: "xProver",
+  tagline: "",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://xprover.com/',
+  url: "https://xprover.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'xProver', // Usually your GitHub org/user name.
-  projectName: 'xProver-website', // Usually your repo name.
+  // organizationName: "xProver", // Usually your GitHub org/user name.
+  // projectName: "xprover-website", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/xprover/xprover-website/blob/main/',
+          editUrl:'https://github.com/xprover/xprover-website/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/xprover/xprover-website/blob/main/',
+          editUrl: 'https://github.com/xprover/xprover-website/blob/main/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -62,69 +60,69 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'xProver',
+        title: "xProver",
         logo: {
-          alt: 'xProver Logo',
-          src: 'img/logo.svg',
+          alt: "xProver Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          { to: '/resources', label: 'Resources', position: 'left'},
-          { to: '/contact', label: 'Contact', position: 'left'},
+          { to: "/blog", label: "Blog", position: "left" },
+          { to: "/resources", label: "Resources", position: "left" },
+          { to: "/contact", label: "Contact", position: "left" },
           {
-            href: 'https://github.com/xprover',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/xprover",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Docs',
-                to: '/docs/intro',
+                label: "Docs",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Github',
-                href: 'https://github.com/xprover',
+                label: "Github",
+                href: "https://github.com/xProver",
               },
               {
-                label: 'Telegram',
-                href: 'https://t.me/xProver',
+                label: "Telegram",
+                href: "https://t.me/xProver",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/xprover',
+                label: "Twitter",
+                href: "https://twitter.com/xProver",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'Resources',
-                href: '/resources',
+                label: "Resources",
+                href: "/resources",
               },
             ],
           },
